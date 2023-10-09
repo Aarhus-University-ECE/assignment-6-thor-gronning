ed0d586739874e24f51139b21de322f5124709eb
Thorispump <143934494+Thorispump@users.noreply.github.com>
2023-10-09 16:36:18 +0200

    Update max.c

--- max.c
+++ max.c
@@ -6,7 +6,21 @@ int max(int *numbers, int size) {
   // Excercise 2
   // Implement your code below...
 
-  return -1;
+	    assert(numbers != NULL);
+    assert(size > 0);
+
+    int max_value = numbers[0];
+
+    for (int i = 1; i < size; i++) {
+        if (numbers[i] > max_value) {
+            max_value = numbers[i];
+        }
+    }
+
+    return max_value;
+
+
+
 }
 
 #ifndef TEST
